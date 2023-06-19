08fb134463a8b025deb23d57777d19ac7270c023
oabt <oabt1996@gmail.com>
2023-06-19 15:06:44 +0800

    1. created the directory for lua configs; 2. add lua configs for 'lspconfig', 'nvim-autopairs', 'nvim-cmp'

---
+++ lua/lspconfig-setup.lua
@@ -0,0 +1,84 @@
+--local lsp_installer = require "nvim-lsp-installer"
+local lspconfig = require('lspconfig')
+
+lspconfig.pylsp.setup{
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          ignore = {'W391','E301','E303','E305','E405','E501','W391','F401','E731','E128','E402','E702','F841','E722'},
+          maxLineLength = 100
+        }
+      }
+    }
+  }
+}
+
+lspconfig.clangd.setup{}
+
+vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+            -- Enable completion triggered by <c-x><c-o>
+            vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+            -- Buffer local mappings.
+            -- See `:help vim.lsp.*` for documentation on any of the below functions
+            local opts = { buffer = ev.buf }
+            vim.keymap.set('n', 'gD', ':sp | vim.lsp.buf.declaration()<cr>', opts)
+            --vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+            vim.keymap.set('n', 'gd', ':sp | lua vim.lsp.buf.definition()<cr>', opts)
+            vim.keymap.set('n', '<C-]>', ':sp | lua vim.lsp.buf.definition()<cr>', opts)
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+            vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+            --vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+            --vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+            --vim.keymap.set('n', '<space>wl', function()
+            --    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+            --end, opts)
+            --vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+            --vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+            --vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+            --vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+            --vim.keymap.set('n', '<space>f', function()
+            --    vim.lsp.buf.format { async = true }
+            --end, opts)
+            vim.keymap.set('n', '<Leader>h', vim.diagnostic.goto_prev)
+            vim.keymap.set('n', '<Leader>l', vim.diagnostic.goto_next)
+            vim.keymap.set('n', '<M-e>', function()
+                    if vim.diagnostic.is_disabled() then
+                        vim.diagnostic.enable()
+                    else
+                        vim.diagnostic.disable()
+                    end
+                end, opts)
+        end
+    })
+
+
+local signs = {
+    {name = "DiagnosticSignError", text = ✖},
+    {name = "DiagnosticSignWarn", text = }
+}
+
+local diagnostic_config = {
+    virtual_text = false,
+    update_in_insert = false,
+    float = {
+        scope = 'buffer'
+    }
+}
+
+vim.diagnostic.config(diagnostic_config)
+
+for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+    vim.api.nvim_set_hl(0, group, {})
+end
+
+vim.cmd([[
+    sign define DiagnosticSignWarn text= texthl=DiagnosticSignWarn linehl= numhl=
+    sign define DiagnosticSignError text=✖ texthl=DiagnosticSignError linehl= numhl=
+    ]]
+)
+
---
+++ lua/nvim-autopairs-setup.lua
@@ -0,0 +1,4 @@
+local autopairs_config = require('nvim-autopairs')
+local pair_rule = require('nvim-autopairs.rule')
+
+autopairs_config.setup{}
