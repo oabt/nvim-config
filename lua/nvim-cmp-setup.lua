7c7522dd5acc7d8d40f4b3ca7dcd58ac4b40cbe9
oabt <oabt1996@gmail.com>
2023-07-22 11:24:03 +0800

    in nvim-cmp-setup.lua: add <S-Tab> mapping to select previous item

--- lua/nvim-cmp-setup.lua
+++ lua/nvim-cmp-setup.lua
@@ -32,6 +32,13 @@ cmp.setup({
                     fallback()
                 end
             end,
+            ['<S-Tab>'] = function(fallback)
+                if cmp.visible() then
+                    cmp.select_prev_item()
+                else
+                    fallback()
+                end
+            end,
             ['<C-n>'] = cmp.mapping(
                 function(fallback)
                     if cmp.visible() then
