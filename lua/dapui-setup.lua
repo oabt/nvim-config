d2a8be39dce110bd75784a74bdec4e89bbbc35ee
oabt <oabt1996@gmail.com>
2023-08-30 15:00:05 +0800

    change the layout of the dapui elements

--- lua/dapui-setup.lua
+++ lua/dapui-setup.lua
@@ -1,7 +1,43 @@
 
 local dapui = require('dapui')
 
-dapui.setup({})
+dapui.setup({
+    layouts = {
+        {
+        elements = { {
+            id = "scopes",
+            size = 0.25
+          }, {
+            id = "watches",
+            size = 0.25
+          } },
+        position = "left",
+        size = 40
+      }, {
+        elements = { {
+            id = "breakpoints",
+            size = 0.5
+          }, {
+            id = "stacks",
+            size = 0.5
+          } },
+        position = "right",
+        size = 30
+      }, {
+        elements = { {
+            id = "repl",
+            size = 0.5
+          }, {
+            id = "console",
+            size = 0.5
+          } },
+        position = "bottom",
+        size = 10
+        }
+    }
+})
+
+-- TODO: mapping for debugger: step-into step-out step-over contiue
 
 vim.cmd([[command! DapToggleUI exec "lua require'dapui'.toggle()"]])
 
