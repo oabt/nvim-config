e30c9bc06912e4fd0a76c09fa5ad97a9ee79dc03
oabt <oabt1996@gmail.com>
2023-11-25 10:08:03 +0800

    in lspconfig_setup.lua: set 'single' border for lsp floating preview.

--- lua/lspconfig_setup.lua
+++ lua/lspconfig_setup.lua
@@ -3,6 +3,14 @@ local lspconfig = require('lspconfig')
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+-- override default open_floating_preview function
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+    opts = opts or {}
+    opts.border = opts.border or "single"
+    return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
+
 lspconfig.pyright.setup{
     capabilities = capabilities,
 }
