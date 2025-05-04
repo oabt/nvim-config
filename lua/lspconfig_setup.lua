58865bdb8f827598d12c441d6c52afdf7fa43d2c
oabt <oabt1996@gmail.com>
2025-05-04 11:21:11 +0800

    in lspconfig_setup.lua: copy the default capabilities from blink.cmp

--- lua/lspconfig_setup.lua
+++ lua/lspconfig_setup.lua
@@ -1,7 +1,53 @@
 --local lsp_installer = require "nvim-lsp-installer"
 local lspconfig = require('lspconfig')
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+-- @oabt: copied from blink.cmp/lua/blink/cmp/sources/lib/init.lua
+local function get_lsp_capabilities(override, include_nvim_defaults)
+  return vim.tbl_deep_extend('force', include_nvim_defaults and vim.lsp.protocol.make_client_capabilities() or {}, {
+    textDocument = {
+      completion = {
+        completionItem = {
+          snippetSupport = true,
+          commitCharactersSupport = false, -- todo:
+          documentationFormat = { 'markdown', 'plaintext' },
+          deprecatedSupport = true,
+          preselectSupport = false, -- todo:
+          tagSupport = { valueSet = { 1 } }, -- deprecated
+          insertReplaceSupport = true, -- todo:
+          resolveSupport = {
+            properties = {
+              'documentation',
+              'detail',
+              'additionalTextEdits',
+              'command',
+              'data',
+              -- todo: support more properties? should test if it improves latency
+            },
+          },
+          insertTextModeSupport = {
+            -- todo: support adjustIndentation
+            valueSet = { 1 }, -- asIs
+          },
+          labelDetailsSupport = true,
+        },
+        completionList = {
+          itemDefaults = {
+            'commitCharacters',
+            'editRange',
+            'insertTextFormat',
+            'insertTextMode',
+            'data',
+          },
+        },
+
+        contextSupport = true,
+        insertTextMode = 1, -- asIs
+      },
+    },
+  }, override or {})
+end
+
+local capabilities = get_lsp_capabilities()
 
 -- override default open_floating_preview function
 -- local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
